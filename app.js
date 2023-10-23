39e946cf0e539697798e2b30ec6b245644d0f845
Karan Sharma <sharmakaran0212@gmail.com>
2023-10-23 14:08:15 +0530

    Completed Week3-Lab2

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home Page' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact US' });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
